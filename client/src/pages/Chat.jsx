978583b800cf98feb3887b6ad33c0c0500eab170
lafathalfath <iqnaraidan12@gmail.com>
2024-01-01 03:11:12 +0700

    working on chat page on client side

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -7,11 +7,24 @@ const Chat=()=>{
     useEffect(()=>{
         axios.get('http://localhost:8000/api/history')
         .then((res)=>{
-        console.log(res.data)
+            const chatdata = res.data.payload
+            setChat(chatdata)
         })
-    })
+    }, [])
+    console.log(chat)
     return <>
         <Link className='text-white' to='/'>back</Link>
+
+        <div>
+            {
+                chat && chat.map(item=>{
+                    return <div key={item.id} className='w-full relative'>
+                        <div className='m-2 p-2 rounded-md font- bg-amber-400 w-fit right-0'>{item.chat}</div>
+                        <div className='m-2 p-2 rounded-md font- bg-gray-200 w-fit'>{item.response}</div>
+                    </div>
+                })
+            }
+        </div>
     </>
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import { FaCaretRight } from "react-icons/fa"
 
 const Home=()=>{
     return <div className="h-[100vh] flex flex-col items-center justify-center">
         <h1 className="text-center text-white text-2xl font-semibold">Ask anything, your assistant is ready to answer. <br /> Click button on below to start conversation</h1>
-        <Link to='/chat' className="text-white text-lg text-center">Start Converstion</Link>
+        <Link to='/chat' className="m-5 py-3 px-6 rounded-full bg-white hover:bg-slate-500 text-gray-800 hover:text-white text-lg text-center font-semibold">Start Converstion <FaCaretRight/></Link>
     </div>
 }
 
